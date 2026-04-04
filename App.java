d556d4bc9384e38f52d87b5c081f722782cbaca3
TechGBP <249761316+TechGBP@users.noreply.github.com>
2026-04-04 12:40:08 -0400

    Add files via upload
    Updated version. each math function has it own class. Try catch blocks were included to show error code for invalid user input. Division has an if statement to check that the second number entered is not zero to make sure that the first number enter is not being divided by zero.

--- App.java
+++ App.java
@@ -5,11 +5,42 @@ Assignment: make the user interactions for the calculator
 */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 
 public class App {
+
+
+    static class Addition { /* used to add user numders together */
+        public double calculate(double num1, double num2) {
+        return num1 + num2;
+    }
+}
+    static class Subtraction {     /* used to subtract user numders from each other */
+        public double calculate(double num1, double num2) {
+        return num1 - num2;
+    }
+}
+
+    static class Multiplication {       /* used to multiply user numders together */
+        public double calculate(double num1, double num2) {
+        return num1 * num2;
+    }
+}
+    static class Division {      /* used to divide a user numder from another number */
+        public double calculate(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Error: Cannot divide by zero.");  
+            return 0;
+        }
+        return num1 / num2;
+    }
+}
+
+
     public static void main(String[] args) throws Exception {
         System.out.println();
-        System.out.println("Gabriela Balanta - Week 1 - 1.4 Project - User Interactions and I/O");
+        System.out.println("Gabriela Balanta - Week 2 - 2.2 Project - Software Design and Control Statements");
         String hello = "Hi! Welcome to Gabriela Balanta's Java calculator";
         System.out.println();
         System.out.println(hello);
@@ -18,18 +49,33 @@ public class App {
         double num1, num2, answer; /* used to store value for user input */
         /* make scanner for user input */
         Scanner scanner = new Scanner(System.in);
+        Addition add = new Addition();
+        Subtraction sub = new Subtraction();
+        Multiplication mul = new Multiplication();
+        Division div = new Division();
+        
 
         while(count>2){  /* loops for repeated use */
             System.out.println("Main Menu");
             System.out.println("Enter the corresponding number for your selection: ");
             System.out.println("\t1 for addition");
-            System.out.println("\t2 for subtraction");
+            System.out.println("\t2 for  subtraction");
             System.out.println("\t3 for multiplication");
             System.out.println("\t4 for division");
             System.out.println("\t5 to exit");
             /* vairable for user choice */
-             int choice = scanner.nextInt(); 
-
+            int choice = 0;
+            
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter a number.");
+                scanner.nextLine(); // clear the bad input
+                continue; // restart loop
+            }
+        if( choice <= 0 || choice > 5){
+            System.out.println("Please enter a value between 1 and 5!");
+        }
         if(choice == 5)
             {
             count=count-7; /*closes while loop to exit */
@@ -38,46 +84,75 @@ public class App {
         if(choice==1)  /* user choose addition */
             {
             System.out.println("You choose addition.");
-            System.out.println("Enter first number (can have decimals):");
-            num1 = scanner.nextDouble();
-            System.out.println("Enter second number(can have decimals):");
-            num2 = scanner.nextDouble();
+            try {
+                System.out.print("Enter first number (can have decimals): ");
+                num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number (can have decimals): ");
+                num2 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid user input! Please enter numbers only.");
+                scanner.nextLine(); /* gets rid of invalid user input */
+            continue; /* returns to menu */
+            }
             System.out.printf("%s + %s = ? %n", num1, num2);
-            answer = num1 + num2;
-            System.out.printf("Answer is %.3f%n%n", answer);
+            /* calls user method for addition */
+            answer = add.calculate(num1, num2);
+            System.out.printf("Answer is %.3f%n%n" + answer);
             }
         if(choice==2) /* user choose subtraction */
             {
             System.out.println("You choose subtraction.");
-            System.out.println("Enter first number(can have decimals):");
-            num1 = scanner.nextDouble();
-            System.out.println("Enter second number(can have decimals):");
-            num2 = scanner.nextDouble();
+            try {
+                System.out.print("Enter first number (can have decimals): ");
+                num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number (can have decimals): ");
+                num2 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid user input! Please enter numbers only.");
+                scanner.nextLine(); 
+            continue; 
+            }
             System.out.printf("%s - %s = ? %n", num1, num2);
-            answer = num1 - num2;
-            System.out.printf("Answer is %.3f%n%n", answer);
+            answer = sub.calculate(num1, num2);
+            System.out.printf("Answer is %.3f%n%n" + answer);
             }
         if(choice==3) /* user choose multiplication */
             {
             System.out.println("You choose multiplication.");
-            System.out.println("Enter first number(can have decimals):");
-            num1 = scanner.nextDouble();
-            System.out.println("Enter second number(can have decimals):");
-            num2 = scanner.nextDouble();
+            try {
+                System.out.print("Enter first number (can have decimals): ");
+                num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number (can have decimals): ");
+                num2 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid user input! Please enter numbers only.");
+                scanner.nextLine(); /* gets rid of invalid user input */
+            continue; /* returns to menu */
+            }
             System.out.printf("%s * %s = ? %n", num1, num2);
-            answer = num1 * num2;
-            System.out.printf("Answer is %.3f%n%n", answer);
+            answer = mul.calculate(num1, num2);
+            System.out.printf("Answer is %.3f%n%n" + answer);
             }
         if(choice==4)   /* user choose division */
             {
             System.out.println("You choose division.");
-            System.out.println("Enter first number(can have decimals):");
-            num1 = scanner.nextDouble();
-            System.out.println("Enter second number(can have decimals):");
-            num2 = scanner.nextDouble();
+            try {
+                System.out.print("Enter first number (can have decimals): ");
+                num1 = scanner.nextDouble();
+
+                System.out.print("Enter second number (can have decimals): ");
+                num2 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid user input! Please enter numbers only.");
+                scanner.nextLine(); /* gets rid of invalid user input */
+            continue; /* returns to menu */
+            }
             System.out.printf("%s / %s = ? %n", num1, num2);
-            answer = num1 / num2;
-            System.out.printf("Answer is %.3f%n%n", answer);
+            answer = div.calculate(num1, num2);
+            System.out.printf("Answer is %.3f%n%n" + answer);
             }
         
         }
